444f1480a445c4e48c46940c7048aba4be7f7f2c
secondstep4 <ds7673@naver.com>
2022-06-19 17:50:14 +0900

    complete

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
-const h1 = document.querySelector("div.hello:first-child h1");
-
-function handleTitleClick(){
-    const clickedClass = "clicked";
-    /*
-    if(h1.classList.contains(clickedClass)){
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
-    */
-    h1.classList.toggle(clickedClass);
-    console.log(h1.classList)
+const list = document.querySelectorAll('.list');
+function activeLink(){
+    list.forEach((item)=>
+    item.classList.remove('active'));
+    this.classList.add('active');
 }
-/*
-function handleMouseEnter(){
-    console.log("mouse is here");
-}
-
-function handleMouseLeave(){
-    console.log("mouse is leave");
-}
-
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-*/
-h1.addEventListener("click", handleTitleClick);
+list.forEach((item)=>item.addEventListener('click',activeLink ));
